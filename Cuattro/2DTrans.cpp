8ce7bb0c0f5b4510935fb833380208120d22a8c5
py-ranoid <vishstar88@gmail.com>
2017-07-24 14:38:52 +0530

    Added Matmul and switch-case

--- Cuattro/2DTrans.cpp
+++ Cuattro/2DTrans.cpp
@@ -1,27 +1,53 @@
-#include
-
-int* matmul(int a[],int b[],int r1,int c1,int r2,int c2)  {
-  int mult[10][10];
-  int i, j, k;
-
-  for(i = 0; i < r1; ++i)
-      for(j = 0; j < c2; ++j)
-          mult[i][j]=0;
-
-  return(mult)
+#include <iostream>
+using namespace std;
+#define NUMPOINTS 4
+#define H 1
+int* matmul(int a[3][3],int b[3][1],int c[3][1])  {
+  int i, j;
+  for (i = 0;i<3;i++)
+      c[i][0]=0;
+  for (i = 0;i<3;i++)
+      for(j=0;j<3;j++)
+          c[i][0]=a[i][j]*b[j][0];
 }
-int main(w)
+int main()
 {
-
-    // Displaying the multiplication of two matrix.
-    cout << endl << "Output Matrix: " << endl;
-    for(i = 0; i < r1; ++i)
-    for(j = 0; j < c2; ++j)
-    {
-        cout << " " << mult[i][j];
-        if(j == c2-1)
-            cout << endl;
+    float prepoints[NUMPOINTS][2];
+    float postpoints[NUMPOINTS][2];
+    float P[3][1], P1[3][1];
+    float T[3][3];
+    int ch;
+    cout << "1. Translation\n2.Rotation\n3.Scale\nChoice : ";
+    cin>>ch;
+    switch (ch) {
+      case 1: for (int i = 0;i < NUMPOINTS;i++){
+                  P[0][0] = prepoints[i][0]*H;
+                  P[1][0] = prepoints[i][1]*H;
+                  P[2][0] = H;
+                  getMatrix(P,P1);
+                  postpoints[i][0] = P1[0][0]/H;
+                  postpoints[i][1] = P1[0][1]/H;
+              }
+              break;
+      case 2: for (int i = 0;i < NUMPOINTS;i++){
+                  P[0][0] = prepoints[i][0]*H;
+                  P[1][0] = prepoints[i][1]*H;
+                  P[2][0] = H;
+                  getMatrix(P,P1);
+                  postpoints[i][0] = P1[0][0]/H;
+                  postpoints[i][1] = P1[0][1]/H;
+              }
+              break;
+      case 3: for (int i = 0;i < NUMPOINTS;i++){
+                  P[0][0] = prepoints[i][0]*H;
+                  P[1][0] = prepoints[i][1]*H;
+                  P[2][0] = H;
+                  getMatrix(P,P1);
+                  postpoints[i][0] = P1[0][0]/H;
+                  postpoints[i][1] = P1[0][1]/H;
+              }
+              break;
+      default:cout<<"Wrong choice";break;
     }
-
     return 0;
 }
